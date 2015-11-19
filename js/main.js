1045cfdb7369b1e10317dd3a463b076afd8f34f9
Loic URIEN <Loic URIEN>
2015-11-19 21:35:47 +0100

    Removed idiot JSON hand parsing ...

--- js/main.js
+++ js/main.js
@@ -8,10 +8,7 @@ var player,
 	movement,
 	collisionLayerIndex,
 	lastMoveDirection,
-	lastCompletedMoveDirection,
-	tileSetProperties;
-
-var collisionData;
+	lastCompletedMoveDirection;
 	
 var animationComplete = true;
 
@@ -95,13 +92,6 @@ DirectionInfos[Phaser.Keyboard.DOWN] = {
 var moving;
 var last;
 
-var getJSONP = function(url){
-	var HttpReq = new XMLHttpRequest();
-	HttpReq.open("GET", url, false);
-	HttpReq.send(null);
-	return HttpReq.responseText;
-}
-
 // Simple function to check if the (x, y) coordinate is in world bound.
 // The coordinate must be in world space.
 var inBound = function(x, y){
@@ -131,9 +121,9 @@ var playerCollideWith = function(direction){
 	var targetY = sprite.y/32 + DirectionInfos[direction].offset.y;
 	// The first condition is only to be sure that we are not passing negative values to the getTile function.
 	// However, going out of bound should never happen, should we leave it here ?
-
+	console.log(map.getTile(targetX, targetY, 'calque', true).properties);
 	return (!inBound(targetX, targetY)
-			|| map.getTile(targetX, targetY, 0, true).properties.collides);
+			|| map.getTile(targetX, targetY, 'calque', true).properties.collides);
 }
 
 // Check if the keyCode passed is a directional key's keyCode.
@@ -183,33 +173,18 @@ function onAnimationComplete(sprite, animation){
 
 // Preload everything before we start.
 // TODO : we should store mapData in memory, not just data
-var data;
+//var data;
 var preload = function () {
-	// Make the tilemap always reload instead of sitting in cache. Useful for testing and map making.
-	var mapData = getJSONP('./assets/map.json?v=' + (new Date()).getTime());
-	game.load.tilemap('map', '', mapData, Phaser.Tilemap.TILED_JSON);
+	// Make the tilemap always reload instead of sitting in cache. Useful for testing and map updating.
+	game.load.tilemap('map', './assets/map.json?v=' + (new Date()).getTime(), null, Phaser.Tilemap.TILED_JSON);
 	game.load.image('Retro_Tileset_RGB', 'assets/Retro_Tileset_RGB.png', 32, 32);
  	game.load.spritesheet('red', 'assets/red.png?v=1', 32, 32);
-	data = JSON.parse(mapData);
-	data = data.tilesets[0].tileproperties;
 };
 
 // On game creation.
 var create = function () {
 	map = game.add.tilemap('map');
 	map.addTilesetImage('Retro_Tileset_RGB');
-	
-	map.tilesets[0].properties = data;
-	tileSetProperties = map.tilesets[0].properties;
-	
-	// The parseint is somewhat ugly here. Well ...
-	for(var key in data){
-		console.log((parseInt(key)+1).toString());
-		map.setCollision((parseInt(key)+1).toString(), true)
-	}
-
-	//collide = map.createLayer('collisions');
-	//collide.resizeWorld();
 
 	layer = map.createLayer('calque');
 	layer.resizeWorld();
